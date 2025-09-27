9902270e54a61b377850b2a70a28baf494f16b79
Anthony Terry <terryant64@gmail.com>
2025-09-27 09:57:16 -0400

    more art, more changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,25 @@ import potaraLogo from './assets/potara-logo.png'
 
 function App() {
   return (
-    <div className="relative h-screen overflow-hidden flex flex-col items-center justify-center">
+    <div className="relative h-screen overflow-hidden flex flex-col items-center justify-start pt-37">
+
       {/* Foreground content */}
       <div className="relative z-10 flex flex-col items-center">
-        <h2 className="text-black text-lg mb-2">Welcome to</h2>
+        <h2 className="text-black text-3xl mb-2">
+          Welcome to
+        </h2>
 
         <img
           src={potaraLogo}
           alt="Potara Logo"
-          className="w-74 mb-2"
+          className="w-103 mb-2"
         />
 
-        <h1 className="text-black text-2xl font-unkempt mb-6">
+        <h1 className="text-black text-3xl font-unkempt mb-6">
           Make Drawing Simple.
         </h1>
 
-        <button className="mt-4 px-6 py-3 bg-white text-black font-semibold rounded-lg shadow-md hover:bg-gray-200 transition">
+        <button className="px-10 py-6 bg-white text-black text-3xl font-semibold rounded-3xl shadow-md hover:bg-gray-200 transition">
           Get Started
         </button>
       </div>
@@ -27,7 +30,7 @@ function App() {
       <img
         src={mountains}
         alt="Mountains"
-        className="absolute bottom-0 left-1/2 -translate-x-1/2 max-w-full z-0"
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[100%] z-0"
       />
     </div>
   )
