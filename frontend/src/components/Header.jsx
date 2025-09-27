6d527b586af81563e8c5eefc73e7178a183e5eda
Anthony Terry <terryant64@gmail.com>
2025-09-27 17:46:02 -0400

    fixed header and start.jsx ui/ux

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,14 @@
-import potaraLogo from '../assets/potaraLogo.png'
+import logo from '../assets/potara-logo.png'
 
-
-function Header() {
-    return (
-        <>
-            <a href="/">
-                <img className="min-w-30 max-w-50 m-0 absolute top-5 left-5" src={potaraLogo} alt="Potara Logo" />
-            </a>
-        </>
-    )
+function Header() 
+{
+  return (
+    <header className="top-0 left-0 right-0 z-50 h-20 bg-beige flex items-center px-5">
+      <a href="/">
+        <img className="w-32" src={logo} alt="Potara Logo" />
+      </a>
+    </header>
+  );
 }
 
 export default Header
