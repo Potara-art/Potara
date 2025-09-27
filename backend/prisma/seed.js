1cd214aa859813886f90c7f6d0ba6898f75c4da6
Faris Abdelrahem <farishabdelrahem@gmail.com>
2025-09-27 19:14:33 -0400

    switch auth0 property to string

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -4,15 +4,16 @@ const prisma = new PrismaClient();
 /* ----------  USER  ---------- */
 const User = [
   /* Patients */
-  { email: "a@test.com", name: "Alice" },
-  { email: "jane.doe@test.com", name: "Jane" },
-  { email: "bart.joe@test.com", name: "Bart" },
-  { email: "johnny.test@example.com", name: "Johnny" },
-  { email: "david.duncan@example.com", name: "David" },
-  { email: "Larry.foster@example.com", name: "Larry" }
+  { email: "a@test.com", name: "Alice", auth0_id: "auth0|test-alice" },
+  { email: "jane.doe@test.com", name: "Jane", auth0_id: "auth0|test-jane" },
+  { email: "bart.joe@test.com", name: "Bart", auth0_id: "auth0|test-bart" },
+  { email: "johnny.test@example.com", name: "Johnny", auth0_id: "auth0|test-johnny" },
+  { email: "david.duncan@example.com", name: "David", auth0_id: "auth0|test-david" },
+  { email: "Larry.foster@example.com", name: "Larry", auth0_id: "auth0|test-larry" }
 ];
 
 async function main() {
+  console.log("Seeding database...");
   for (const user of User) {
     await prisma.user.create({ data: user });
   }
@@ -20,8 +21,10 @@ async function main() {
   console.log("Database seeded successfully!");
 }
 
-main().then(() => prisma.$disconnect()).catch(e => {
-  console.error(e);
-  prisma.$disconnect();
-  process.exit(1);
-});
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
