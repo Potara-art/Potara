aee299e5002947423718f991a4556a8df2f1850e
Anthony Terry <terryant64@gmail.com>
2025-09-28 02:48:40 -0400

    started adding gallery page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import App from './App'
 import Start from './Start/start.jsx'
 import SignUp from './auth/sign-up.jsx'
 import LogIn from './auth/log-in.jsx'
+import Gallery from './profile/gallery.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/start" element={<Start />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/log-in" element={<LogIn />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
