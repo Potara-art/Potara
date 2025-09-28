0f8a0d2d03fedeaf914086df06d53f97d9983260
Thomas Trivino <th992984@ucf.edu>
2025-09-28 01:44:29 -0400

    Reformat

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
-
 import App from "./App";
 import Start from "./Start/Start.jsx";
 import SignUp from "./auth/sign-up.jsx";
