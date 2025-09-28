ec648b8adc53d85ac073072fa1ab84e32bbee639
Thomas Trivino <th992984@ucf.edu>
2025-09-28 01:35:37 -0400

    Corrects naming of Start.jsx:

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 // main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
-import App from './App'
-import Start from './Start/start.jsx'
-import SignUp from './auth/sign-up.jsx'
-import LogIn from './auth/log-in.jsx'
-import './index.css'
+import App from "./App";
+import Start from "./Start/Start.jsx";
+import SignUp from "./auth/sign-up.jsx";
+import LogIn from "./auth/log-in.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
@@ -23,4 +23,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
-)
+);
